111e53b1cf11e173c731c906ee89d49afb4146d5
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-07-24 18:27:44 -0400

    Time: 556 ms (76.67%), Space: 45.4 MB (27.54%) - LeetHub

---
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} matrix
+ * @param {number} target
+ * @return {boolean}
+ */
+var searchMatrix = function(matrix, target) {
+    let m = matrix.length, n = matrix[0].length
+    let i = 0, j = n-1
+    while(i >= 0 && i < m && j >= 0 && j < n) {
+        if(target == matrix[i][j]) {
+            return true
+        } else if(target < matrix[i][j]) {
+            j--
+        } else {
+            i++
+        }
+    }
+    return false
+};
