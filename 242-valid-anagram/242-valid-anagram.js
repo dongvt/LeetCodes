c88d191f3ced17b5801aa07c3fad9b0b5c9761f9
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-07-27 20:57:26 -0400

    Time: 79 ms (96.23%), Space: 42.1 MB (97.58%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+    if(s.length !== t.length) return false;
+    const charArray = new Array(26).fill(0);
+    for(let i = 0 ; i < s.length ; i++) {
+        charArray[s.charCodeAt(i) - 97]++;
+        charArray[t.charCodeAt(i) - 97]--;
+    }
+    
+    for(const n of charArray){
+        if(n !== 0) return false;
+    }
+    
+    return true;
+    
+};
