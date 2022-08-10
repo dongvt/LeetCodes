18dd4f3fa156fae6dfa484f77123c7ee00132338
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-08-10 19:49:41 +0100

    Time: 98 ms (51.67%), Space: 44 MB (49.37%) - LeetHub

---
+++ 314-binary-tree-vertical-order-traversal/314-binary-tree-vertical-order-traversal.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var verticalOrder = function(root) {
+    if(root === null) return [];
+    const map = new Map();
+    
+    const q = new Queue();
+    
+    q.enqueue([root,0]);
+    while(!q.isEmpty()) {
+        const [node,idx] = q.dequeue();
+        
+        if(map.has(idx)) map.get(idx).push(node.val);
+        else map.set(idx,[node.val]);
+        
+        if(node.left !== null) q.enqueue([node.left,idx - 1]);
+        if(node.right !== null) q.enqueue([node.right,idx + 1]);
+    }
+    
+    
+    return [...map].sort((a,b) => a[0] - b[0]).map(item => item[1]);
+};
