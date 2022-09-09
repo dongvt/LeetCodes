3d88b307a1a96b75896cd7b13df9bae099ab1392
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-09-09 02:27:54 +0100

    Time: 198 ms (34.16%), Space: 130.8 MB (53.13%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int numberOfWeakCharacters(int[][] properties) {
+        Arrays.sort(properties,(a,b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]);
+        
+        int maxDefence = 0;
+        int count = 0;
+        for(int i = properties.length - 1; i >= 0  ; i--) {
+            if(properties[i][1] < maxDefence) {
+                count++;
+            }
+            maxDefence = Math.max(maxDefence,properties[i][1]);
+        }
+        
+        return count;
+    }
+}
