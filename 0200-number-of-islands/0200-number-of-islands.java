af98ad3ef97b551a43a3c00c1c5626fac149b1b9
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-11-16 15:39:37 -0500

    Time: 11 ms (37.96%), Space: 57.7 MB (34.11%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -0,0 +1,31 @@
+class Solution {
+    char [][] board;
+    
+    private void dfs(int x, int y) {
+        if(x < 0 || x >= board.length || y < 0 || y >= board[0].length
+          || board[x][y] == '0') {
+            return;
+        }
+        this.board[x][y] = '0';
+        this.dfs(x+1,y);
+        this.dfs(x-1,y);
+        this.dfs(x,y+1);
+        this.dfs(x,y-1);
+    }
+    
+    public int numIslands(char[][] grid) {
+        this.board = grid;
+        int count = 0;
+        for(int i = 0 ; i < grid.length; i++) {
+            for(int j = 0; j < grid[0].length ; j++) {
+                if(grid[i][j] == '1') {
+                    dfs(i,j);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
+
