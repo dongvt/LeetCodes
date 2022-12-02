70cbaa77f077c885c3578693f9ab960347c8b612
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2022-12-01 20:05:53 -0500

    Time: 1 ms (95.34%), Space: 42 MB (62.01%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,25 +1,39 @@
 class Solution {
-    
-    private String validated(String str) {
-        Stack<Character> stack = new Stack<>();
-        for(int i = 0; i < str.length() ; i++) {
-            if(stack.isEmpty() && str.charAt(i) == '#') continue;
-            
-            if(str.charAt(i) == '#') stack.pop();
-            else stack.push(str.charAt(i));
-        }
-        
-        //System.out.println(stack.toString());
-        
-//         StringBuilder sb = new StringBuilder();
-        
-//         for(char c : stack) {
-//             sb.append(c);
-//         }
-        return stack.toString();
-    }
-    
     public boolean backspaceCompare(String s, String t) {
-        return validated(s).equals(validated(t));
+        int i = s.length() - 1, j = t.length() - 1;
+        int skipS = 0, skipT = 0;
+        
+        while(i >= 0 || j >= 0) {
+            while(i >= 0) {
+                if(s.charAt(i) =='#') {
+                    skipS++;
+                    i--;
+                } else if(skipS > 0) {
+                    i--;
+                    skipS--;
+                } else {
+                    break;
+                }
+            }
+            
+            while(j >= 0) {
+                if(t.charAt(j) == '#') {
+                    skipT++;
+                    j--;
+                } else if(skipT > 0) {
+                    j--;
+                    skipT--;
+                } else {
+                    break;
+                }
+            }
+            
+            if((i >= 0 && j >= 0) && s.charAt(i) != t.charAt(j)) return false;
+            if((i < 0) != (j < 0)) return false;
+            
+            i--; j--;
+            
+        }
+        return true;
     }
 }
