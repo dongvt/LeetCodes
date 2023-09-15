75d6c39de38a3b70fb7525e260b79b50e4947d4d
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2023-09-15 14:00:50 -0400

    Time: 43 ms (95.39%), Space: 41.2 MB (98.88%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.js
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -7,17 +7,16 @@
  */
 var merge = function(nums1, m, nums2, n) {
     let x = m-1, y = n-1, z = m+n-1;
-    while(x >= 0 || y >= 0) {
-        if(x < 0) {
-            nums1[z--] = nums2[y--];
-            continue;
-        }
-        if(y < 0) {
-            nums1[z--] = nums1[x--];
-            continue;
-        }
-        if(nums1[x] > nums2[y]) {
-            //console.log(nums1[x] ,nums2[y])
+    while(z >= 0) {
+        // if(x < 0) {
+        //     nums1[z--] = nums2[y--];
+        //     continue;
+        // }
+        // if() {
+        //     nums1[z--] = nums1[x--];
+        //     continue;
+        // }
+        if(nums1[x] > nums2[y] || y < 0) {
             nums1[z--] = nums1[x--];
         } else {
             nums1[z--] = nums2[y--];
