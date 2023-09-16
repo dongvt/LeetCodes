f33d9e0d770309a6f650ff278539766dcb7e310c
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2023-09-16 11:53:49 -0400

    Time: 123 ms (13.25%), Space: 44.5 MB (67.11%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -3,18 +3,13 @@
  * @return {number}
  */
 var removeDuplicates = function(nums) {
-    const map = new Map();
+
     let idx = 0;
-    for(let i = 0 ; i < nums.length ; i++) {
-        if(!map.has(nums[i])) {
-            map.set(nums[i],idx++);
+    for(let i = 1 ; i < nums.length; i++) {
+        if(nums[idx] !== nums[i]) {
+            nums[++idx] = nums[i];
         }
     }
-    //console.log(map)
-    for(const [k,v] of map) {
-        //console.log(k,v)
-        nums[v] = k;
-    }
 
-    return map.size;
+    return idx + 1;
 };
