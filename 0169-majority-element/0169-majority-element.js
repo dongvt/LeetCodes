3261009c08f18541b66cfcabe490122a4518ad33
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2023-09-17 01:01:07 -0400

    Time: 52 ms (88.27%), Space: 43.5 MB (72.29%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    let candidate = nums[0];
+    let count = 0; 
+    for (let i = 0; i < nums.length ; i++) {
+        
+        if(candidate === nums[i]) count++;
+        else count--;
+        if(count < 0) {
+            candidate = nums[i];
+            count = 0;
+        }
+    }
+
+    return candidate;
+};
