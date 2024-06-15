6ac4e61149dce326e49639dda03bd155fc5aa6c5
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2024-06-15 19:39:42 -0400

    Time: 225 ms (73.64%), Space: 83.2 MB (58.91%) - LeetHub

---
+++ 0502-ipo/0502-ipo.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} k
+ * @param {number} w
+ * @param {number[]} profits
+ * @param {number[]} capital
+ * @return {number}
+ */
+var findMaximizedCapital = function(k, w, profits, capital) {
+    const projects = [];
+    const n = profits.length;
+    for(let i = 0 ; i < n; i++) {
+        projects.push({
+            profit: profits[i],
+            cap: capital[i]
+        });
+    }
+
+    projects.sort((a,b) => a.cap - b.cap);
+
+    const pq = new PriorityQueue({
+        compare: (a,b) => {
+            return b - a;
+        }
+    })
+    let i = 0
+    for(let x = 0; x < k; x++) {
+        for(; i < n && w >= projects[i].cap;i++) {
+            pq.enqueue(projects[i].profit);
+        }
+        if(pq.isEmpty()) break;
+        //console.log(pq.front(),w)
+        w += pq.dequeue();
+    }
+
+    return w;
+
+
+};
