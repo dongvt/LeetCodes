f85419869cd419e38e872ca8a529126debd9d661
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2024-06-26 23:52:15 -0400

    Time: 91 ms (46.1%), Space: 63.9 MB (91.49%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.js
@@ -3,8 +3,9 @@
  * @return {number}
  */
 var findCenter = function(edges) {
-    const set = new Set(edges[0]);
-    for( const n of edges[1]) {
-        if(set.has(n)) return n;
-    }
+    // const set = new Set(edges[0]);
+    // for( const n of edges[1]) {
+    //     if(set.has(n)) return n;
+    // }
+    return edges[0][0] == edges[1][0] || edges[0][0] == edges[1][1] ? edges[0][0] : edges[0][1];
 };
