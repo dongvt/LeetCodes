c886de805eebffe942173b65b21764921e683acd
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-04-24 01:37:49 -0400

    Time: 194 ms (25.96%), Space: 93.7 MB (24.42%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.js
@@ -3,21 +3,19 @@
  * @return {number}
  */
 var longestPalindromeSubseq = function(s) {
-    const lcs = (t1,t2) => {
-        const memo = new Array(t1.length).fill().map(_ => new Array(t2.length).fill(-1))
-        const dp = (i,j) => {
-            if(i >= t1.length || j >= t2.length) return 0;
-            if(memo[i][j] !== -1) return memo[i][j];
-            if(t1.charAt(i) === t2.charAt(j)) {
-                memo[i][j] = dp(i+1,j+1) + 1;
-            } else {
-                memo[i][j] = Math.max(dp(i+1,j),dp(i,j+1));
-            }
-            return memo[i][j];
+    const r = s.split('').reverse().join('');
+    const memo = new Array(r.length).fill().map(_ => new Array(r.length).fill(-1));
+    const LCS = (i,j) => { //i => s, j => r
+        if(i >= s.length) return 0;
+        if(j >= r.length) return 0;
+        if(memo[i][j] !== -1) return memo[i][j];
+        if(s[i] === r[j]) {
+            memo[i][j] = LCS(i + 1, j + 1) + 1;
+        } else {
+            memo[i][j] = Math.max(LCS(i, j + 1),LCS(i + 1, j));
         }
-
-        return dp(0,0);
+        return memo[i][j];
     }
 
-    return lcs(s,s.split('').reverse().join(''))
+    return LCS(0,0);
 };
