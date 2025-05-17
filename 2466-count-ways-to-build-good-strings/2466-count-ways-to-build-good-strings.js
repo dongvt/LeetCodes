09e27990a2df5448f9ea9809c7b11cc2ac66a39e
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-05-17 14:52:53 -0400

    Time: 68 ms (17.95%), Space: 67.2 MB (28.21%) - LeetHub

--- 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.js
+++ 2466-count-ways-to-build-good-strings/2466-count-ways-to-build-good-strings.js
@@ -5,16 +5,19 @@
  * @param {number} one
  * @return {number}
  */
-var countGoodStrings = function(low, high, zero, one) {
-    const memo = new Array(high + 1).fill(-1);
+var countGoodStrings = function (low, high, zero, one) {
+
+    const dp = new Array(high + 1);
     const mod = 10 ** 9 + 7;
-    const dp = (size) => {
-        if(size > high) return 0;
-        if(memo[size] !== -1) return memo[size];
-        const current = size >= low ? 1 : 0;
-        memo[size] = (dp(size + zero) + dp(size + one) + current) % mod;
-        return memo[size];
+    dp[0] = 1;
+    for(let i = 1 ; i < dp.length; i++) {
+        const zeros = dp[i - zero] || 0;
+        const ones = dp[i - one] || 0;
+        dp[i] = (zeros + ones) % mod;
     }
 
-    return dp(0,0);
+    return dp.reduce((acc,curr,i) => {
+        if(i >= low && i <= high) return (acc + curr) % mod;
+        return acc;
+    },0);
 };
