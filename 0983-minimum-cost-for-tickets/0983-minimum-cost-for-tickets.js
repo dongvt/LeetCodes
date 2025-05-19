2e05d22a9e06b104e68831a24e3e005f764301dd
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-05-19 16:50:34 -0400

    Time: 3 ms (52.23%), Space: 54.1 MB (74.52%) - LeetHub

--- 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.js
+++ 0983-minimum-cost-for-tickets/0983-minimum-cost-for-tickets.js
@@ -4,21 +4,32 @@
  * @return {number}
  */
 var mincostTickets = function(days, costs) {
-    const maxDay = Math.max(...days);
-    const dp = new Array(maxDay + 1).fill(0);    
-    const travelDay = new Set(days);
-    
-    
-    for(let i = 1 ; i <= maxDay; i++) {
-        const d7 = Math.max(0, i - 7);
-        const d30 = Math.max(0, i - 30);
-        if(travelDay.has(i))
-            dp[i] = Math.min(dp[i-1] + costs[0],
-                            dp[d7] + costs[1],
-                            dp[d30] + costs[2]);
-        else
-            dp[i] = dp[i-1];
+    //Pick between the three options the cheaper one
+    //Need to check if a range can cover multiple days
+    //To know if a day is covered in a range, we just add the ticket size in days to the current index
+    //We recurse from the first index which days[idx] > last in range
+    //Eg a 7 day ticket in day 2 will range from 2 to 9, we get the index of smaller number > 9
+
+    const [d1,d7,d30] = costs;
+    const nextIdx = (start,target) => {
+        let i = start;
+        while(days[i] <= target && i < days.length) {
+            i++;
+        }
+        return i;
     }
 
-    return dp.at(-1);
+    const memo = new Array(366).fill(-1);
+    const dp = (i) => {
+        if(i >= days.length) return 0;
+        if(memo[i] !== -1) return memo[i];
+        const j = nextIdx(i,days[i] + 6);
+        const k = nextIdx(i,days[i] + 29);
+
+        memo[i] = Math.min(dp(i+1) + d1,dp(j) + d7, dp(k) + d30);
+
+        return memo[i];
+    }
+
+    return dp(0);
 };
