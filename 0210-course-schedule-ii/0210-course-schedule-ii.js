50d15d8db5595705e28a80ff8f0e32ad819e5e1c
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-06-16 20:35:06 -0400

    Time: 11 ms (59.3%), Space: 63.1 MB (22.7%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.js
+++ 0210-course-schedule-ii/0210-course-schedule-ii.js
@@ -3,41 +3,36 @@
  * @param {number[][]} prerequisites
  * @return {number[]}
  */
-var findOrder = function (n, prerequisites) {
-    const graph = Array.from({ length: n }, () => []);
-    const inDegree = Array(n).fill(0);
+var findOrder = function(n, prerequisites) {
+    const visited = Array(n).fill(0);
+    const graph = Array.from({length:n},() => [])
 
-    for (const [a, b] of prerequisites) {
-        inDegree[a]++;
-        graph[b].push(a);
+    for(const [a,b] of prerequisites) {
+        graph[a].push(b);
     }
 
-    //Use BFS to build the response array, enqueue all indegree 0
-    //Use inDegree to keep track of loops (if indegree for a node is 0 then we have a loop)
-    //Each time we dequeue, we push that node into the response
-    const q = new Queue();
-    const visited = Array(n).fill(false);
+    let cycle = false;
     const res = [];
-    for (let i = 0; i < n; i++) {
-        if (inDegree[i] === 0) { 
-            q.enqueue(i); 
+    const dfs = (node) => {
+        if(cycle) return;
+        if(visited[node] === 2) return;
+        if(visited[node] === 1) {
+            cycle = true;
+            return;
         }
-    }
-
-    while (q.size() > 0) {
-        const node = q.dequeue();
+        visited[node] = 1;
+        for(const neigh of graph[node]) {
+            dfs(neigh);
+        }
+        visited[node] = 2;
         res.push(node);
-
-        for (const neighbor of graph[node]) {
-            inDegree[neighbor]--;
-            if(inDegree[neighbor] === 0) q.enqueue(neighbor)
-        }
     }
 
-    if(res.length !== n) return [];
+    for(let i = 0; i < n ; i++) {
+        if(visited[i] === 0) dfs(i);
+        if(cycle) return [];
+    }
 
     return res;
-
-
 
 };
