da0dd01e941b62e1a49a6d20f3a32c50a8a6fa61
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-06-17 18:44:27 -0400

    Time: 712 ms (12.11%), Space: 158.2 MB (28.77%) - LeetHub

--- 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.js
+++ 1857-largest-color-value-in-a-directed-graph/1857-largest-color-value-in-a-directed-graph.js
@@ -5,7 +5,7 @@
  */
 var largestPathValue = function(colors, edges) {
     const n = colors.length;
-    const colorCount = Array(26).fill(0);
+    const colorCount = Array.from({length: n}, () => Array(26).fill(0));;
 
     const graph = Array.from({length: n}, () => []);
 
@@ -13,31 +13,35 @@ var largestPathValue = function(colors, edges) {
         graph[a].push(b);
     }
     const visited = Array(n).fill(0); //0 unvisited 1 visiting 2 visited
-    let res = 0, cycle = false;
+    let cycle = false;
     const dfs = (node) => {
-        if(graph[node].length === 0) {
-            colorCount[colors.charCodeAt(node) - 97]++;
-            res = Math.max(res,...colorCount);
-            colorCount[colors.charCodeAt(node) - 97]--;
-            return;
-        }
-        if(cycle) return;
+        if(cycle || visited[node] === 2) return;
         if(visited[node] === 1) {
             cycle = true;
             return;
         }
         visited[node] = 1;
-        colorCount[colors.charCodeAt(node) - 97]++;
         for(const neighbor of graph[node]) {
             dfs(neighbor);
+            for(let c = 0 ; c < 26; c++) {
+                colorCount[node][c] = Math.max(colorCount[node][c],colorCount[neighbor][c])
+            }
         }
-        colorCount[colors.charCodeAt(node) - 97]--;
+        colorCount[node][colors.charCodeAt(node) - 97]++;
         visited[node] = 2;
     }
 
     for(let i = 0 ; i  < n; i++) {
         if(visited[i] === 0) dfs(i);
         if(cycle) return -1;
+    }
+
+    let res = 0;
+
+    for(let i = 0 ; i < n; i ++) {
+        for(let j = 0; j < 26; j++) {
+            res = Math.max(res,colorCount[i][j]);
+        }
     }
 
     return res;
