7896f4d1d3de5c5af2199654b69848f2e7acaf73
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-06-19 14:18:58 -0400

    Time: 9 ms (6.16%), Space: 61.3 MB (5.03%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.js
@@ -2,13 +2,23 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let profit = 0;
-    for(let i = 1; i < prices.length ; i++) {
-        if(prices[i-1] < prices[i]) {
-            profit += prices[i] - prices[i-1];
+var maxProfit = function (prices) {
+    const n = prices.length;
+    const memo = Array.from({length:n},() => Array(2).fill(-1))
+    const dp = (i, buy) => {
+        if (i >= n) return 0;
+        const key = buy ? 1 : 0;
+        if(memo[i][key] !== -1) return memo[i][key];
+        let res = 0;
+        if (buy) { //Selling
+            res = Math.max(dp(i + 1, false) + prices[i], dp(i + 1, true));
+        } else {// buy
+            res = Math.max(dp(i + 1, true) - prices[i], dp(i + 1, false));
         }
+
+        memo[i][key] = res;
+        return res;
     }
 
-    return profit;
+    return dp(0,false);
 };
