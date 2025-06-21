ef0280b8c4db286db24ff77dcd4f399d83514df0
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-06-20 23:26:33 -0400

    Time: 422 ms (5.1%), Space: 58.1 MB (11.14%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,15 +3,20 @@
  * @return {number}
  */
 var jump = function(nums) {
-    let farest = 0;
-    let end = 0;
-    let res = 0;
-    for(let i = 0 ; i < nums.length - 1; i++) { 
-        farest = Math.max(farest, i + nums[i]);
-        if(i === end) {
-            end = farest;
-            res++;
+    const n = nums.length;
+    const memo = Array(n).fill(-1);
+    const dp = (idx) => {
+        
+        if(idx >= n - 1) return 0;
+        if(memo[idx] !== -1) return memo[idx];
+        let res = Infinity;
+        for(let i = idx + 1; i < idx + nums[idx] + 1; i++) {
+            res = Math.min(res,dp(i) + 1);
         }
+
+        memo[idx] = res;
+        return res;
     }
-    return   res ;
+
+    return dp(0);
 };
