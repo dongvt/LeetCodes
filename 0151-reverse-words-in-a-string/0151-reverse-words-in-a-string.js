ea30e18e0cbe0ca0e9e9564694dc755f2780e722
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-06-24 18:07:44 -0400

    Time: 2 ms (60.92%), Space: 57.2 MB (15.51%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.js
@@ -3,16 +3,6 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-
-    const arrW = s.split(' ')
-    //add words in reverse
-    const res = [];
-    for(let i = arrW.length - 1; i >= 0; i--) {
-        if(arrW[i] !== '') {
-            res.push(arrW[i]);
-        }
-    }
-
-    return res.join(' ');
-    
+    const arr = s.split(' ');
+    return arr.filter(it => it !== '').reverse().join(' ')
 };
