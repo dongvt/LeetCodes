527be7991aeea497e9cbd7d9fe5133c145acd012
Govert Carreno <32555486+dongvt@users.noreply.github.com>
2025-07-07 16:59:27 -0400

    Time: 0 ms (100%), Space: 55.1 MB (43.14%) - LeetHub

--- 0001-two-sum/0001-two-sum.js
+++ 0001-two-sum/0001-two-sum.js
@@ -5,14 +5,10 @@
  */
 var twoSum = function(nums, target) {
     const map = new Map();
-    for(let i = 0; i < nums.length; i++) {
+    for(let i = 0 ; i < nums.length; i++) {
         const n = nums[i];
-        if(map.has(n)) {
-            return [map.get(n),i];
-        } else {
-            map.set(target - n,i)
-        }
+        if(map.has(target - n)) return [map.get(target - n),i];
+        
+        map.set(n,i);
     }
-
-    return null;
 };
